377f55a419c7b7fa09b642fed917a768bcfa6174
christine gomez <christine.gomez1226@gmail.com>
2018-09-06 15:48:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  i = 0
+  while i < dwarves.length
+    puts " #{i+1}. #{dwarves[i]}"
+    i +=1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  new_veggie = []
+  i = 0
+  while i < veggies.length
+    new_veggie << veggies[i].capitalize + "!"
+    i +=1
+  end
+  new_veggie
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  i = 0
+  if planeteer_calls.any? {|i| i.length > 4}
+    return true
+  else
+  end
 end
 
 def find_the_cheese# code an argument here
