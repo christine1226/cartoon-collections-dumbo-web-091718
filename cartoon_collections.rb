237dac3c2196432b061e7113b3e829054e3e204a
christine gomez <christine.gomez1226@gmail.com>
2018-09-06 16:07:45 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ def long_planeteer_calls(planeteer_calls)
 end
 
 def find_the_cheese(cheese_types)
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types = no_cheese
    cheese_types.find do |type|
     cheese_types.include?(type)
   end
